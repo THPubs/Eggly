2b0469c49d6f30192b5eb8d267c7780fa5aa2102
Pubudu Kodikara <pubudu.th@gmail.com>
2015-06-18 06:25:34 +0530

    Simple states complete

--- app/eggly-app.js
+++ app/eggly-app.js
@@ -1,78 +1,121 @@
 angular.module('Eggly', [
 
-  ])
-  .controller('MainCtrl', function($scope) {
-    $scope.categories = [{
-      "id": 0,
-      "name": "Development"
-    }, {
-      "id": 1,
-      "name": "Design"
-    }, {
-      "id": 2,
-      "name": "Exercise"
-    }, {
-      "id": 3,
-      "name": "Humor"
-    }];
+    ])
+    .controller('MainCtrl', function($scope) {
+        $scope.categories = [{
+            "id": 0,
+            "name": "Development"
+        }, {
+            "id": 1,
+            "name": "Design"
+        }, {
+            "id": 2,
+            "name": "Exercise"
+        }, {
+            "id": 3,
+            "name": "Humor"
+        }];
 
-    $scope.bookmarks = [{
-      "id": 0,
-      "title": "AngularJS",
-      "url": "http://angularjs.org",
-      "category": "Development"
-    }, {
-      "id": 1,
-      "title": "Egghead.io",
-      "url": "http://egghead.io",
-      "category": "Development"
-    }, {
-      "id": 2,
-      "title": "A List Apart",
-      "url": "http://alistapart.com/",
-      "category": "Design"
-    }, {
-      "id": 3,
-      "title": "One Page Love",
-      "url": "http://onepagelove.com/",
-      "category": "Design"
-    }, {
-      "id": 4,
-      "title": "MobilityWOD",
-      "url": "http://www.mobilitywod.com/",
-      "category": "Exercise"
-    }, {
-      "id": 5,
-      "title": "Robb Wolf",
-      "url": "http://robbwolf.com/",
-      "category": "Exercise"
-    }, {
-      "id": 6,
-      "title": "Senor Gif",
-      "url": "http://memebase.cheezburger.com/senorgif",
-      "category": "Humor"
-    }, {
-      "id": 7,
-      "title": "Wimp",
-      "url": "http://wimp.com",
-      "category": "Humor"
-    }, {
-      "id": 8,
-      "title": "Dump",
-      "url": "http://dump.com",
-      "category": "Humor"
-    }];
+        $scope.bookmarks = [{
+            "id": 0,
+            "title": "AngularJS",
+            "url": "http://angularjs.org",
+            "category": "Development"
+        }, {
+            "id": 1,
+            "title": "Egghead.io",
+            "url": "http://egghead.io",
+            "category": "Development"
+        }, {
+            "id": 2,
+            "title": "A List Apart",
+            "url": "http://alistapart.com/",
+            "category": "Design"
+        }, {
+            "id": 3,
+            "title": "One Page Love",
+            "url": "http://onepagelove.com/",
+            "category": "Design"
+        }, {
+            "id": 4,
+            "title": "MobilityWOD",
+            "url": "http://www.mobilitywod.com/",
+            "category": "Exercise"
+        }, {
+            "id": 5,
+            "title": "Robb Wolf",
+            "url": "http://robbwolf.com/",
+            "category": "Exercise"
+        }, {
+            "id": 6,
+            "title": "Senor Gif",
+            "url": "http://memebase.cheezburger.com/senorgif",
+            "category": "Humor"
+        }, {
+            "id": 7,
+            "title": "Wimp",
+            "url": "http://wimp.com",
+            "category": "Humor"
+        }, {
+            "id": 8,
+            "title": "Dump",
+            "url": "http://dump.com",
+            "category": "Humor"
+        }];
 
-    $scope.currentCetegory = null;
+        $scope.currentCetegory = null;
 
-    function setCurrentCategory(category) {
-      $scope.currentCetegory = category;
-    }
+        function setCurrentCategory(category) {
+            $scope.currentCetegory = category;
 
-    function isCurrentCategory(category){
-      return $scope.currentCetegory !== null && category.name === $scope.currentCetegory.name;
-    }
+            cancelCreating();
+            cancelEditing();
+        }
 
-    $scope.setCurrentCategory = setCurrentCategory;
-    $scope.isCurrentCategory = isCurrentCategory;
-  });
+        function isCurrentCategory(category) {
+            return $scope.currentCetegory !== null && category.name === $scope.currentCetegory.name;
+        }
+
+        $scope.setCurrentCategory = setCurrentCategory;
+        $scope.isCurrentCategory = isCurrentCategory;
+
+        // ---------------------------------------------------------------------------------------------
+        // Creating and editing states
+        //----------------------------------------------------------------------------------------------
+        $scope.isCreating = false;
+        $scope.isEditing = false;
+
+        function startCreating() {
+            $scope.isCreating = true;
+            $scope.isEditing = false;
+        }
+
+        function cancelCreating() {
+            $scope.isCreating = false;
+        }
+
+        function startEditing() {
+            $scope.isCreating = false;
+            $scope.isEditing = true;
+        }
+
+        function cancelEditing() {
+            $scope.isEditing = false;
+        }
+
+        function shouldShowCreating() {
+            return $scope.currentCetegory && !$scope.isEditing;
+        }
+
+        function shouldShowEditing() {
+            return $scope.isEditing && !$scope.isCreating;
+        }
+
+        $scope.startCreating = startCreating;
+        $scope.cancelCreating = cancelCreating;
+        $scope.startEditing = startEditing;
+        $scope.cancelEditing = cancelEditing;
+        $scope.shouldShowEditing = shouldShowEditing;
+        $scope.shouldShowCreating = shouldShowCreating;
+
+    });
